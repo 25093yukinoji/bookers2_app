74dd9335f39b67c32909da32a823dfc78d1f7998
25093yukinoji <ten.yuki.0601@gmail.com>
2021-01-25 21:22:17 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
- devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  get 'users/show'
+ devise_for :users
  root to: 'homes#top'
  get 'homes/about' => 'homes#about'
- resources :books, only: [:new, :create, :index, :show, :destroy]
+ resources :books
+ resources :users
 end
