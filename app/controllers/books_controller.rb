0e0503f13aff378a8924d0777519a53b91be2781
25093yukinoji <ten.yuki.0601@gmail.com>
2021-01-26 09:38:37 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,62 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new(book_params)
+  def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "Book was successfully created."
+      flash[:notice]="You have creatad book successfully."
       redirect_to book_path(@book)
     else
+      @user = current_user
       @books = Book.all
       render :index
     end
   end
 
-  def index
-    @books = Book.page(params[:page]).reverse_order
-    @book = Book.new
-  end
-
-  def show
-    @book = Book.find(params[:id])
-  end
-
   def edit
-    @book =Book.find(params[:id])
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
+    @book.user_id = current_user.id
     if @book.update(book_params)
-      flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
-      @books = Book.all
-      render :index
+      render :edit
     end
   end
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 
-  def book_params
-    params.permit(:title, :body)
-  end
 
 end
