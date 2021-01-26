5bc41291f01c0945224ba4149e1d3c5c47c7cc9d
25093yukinoji <ten.yuki.0601@gmail.com>
2021-01-26 08:48:38 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  get 'users/show'
- devise_for :usersrai
+ devise_for :users
  root to: 'homes#top'
  get 'homes/about' => 'homes#about'
  resources :books
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_072235) do
+ActiveRecord::Schema.define(version: 2021_01_26_084644) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2021_01_25_072235) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "introduction"
+    t.integer "book_id"
+    t.string "profile_image"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
