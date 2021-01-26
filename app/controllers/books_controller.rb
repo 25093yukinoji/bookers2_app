3ce68af3466c6f60e8994b05815554ce8528eb37
25093yukinoji <ten.yuki.0601@gmail.com>
2021-01-26 07:41:41 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,55 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new(book_params)
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @books = Book.page(params[:page]).reverse_order
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book =Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
- devise_for :users
+ get 'users/show'
+ devise_for :usersrai
  root to: 'homes#top'
  get 'homes/about' => 'homes#about'
  resources :books
