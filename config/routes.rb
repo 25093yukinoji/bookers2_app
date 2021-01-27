15ffc363c546e265bac2b30aa79e7b234739b591
25093yukinoji <ten.yuki.0601@gmail.com>
2021-01-27 10:39:41 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
  get 'users/show'
  devise_for :users
- root to: 'homes#top'
- get 'homes/about' => 'homes#about'
+ root to: 'home#top'
+ get 'home/about' => 'home#about'
  resources :books
  resources :users
 end
